7f5e7e2c21038401e1c3c2a5219dff3359d16a13
9nasghost <9nasghost@gmail.com>
2017-08-25 11:35:10 +0100

    Add files via upload

---
+++ 9nas.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+banner = '''
+
+
+
+ ██████╗ ██╗ █████╗ ███╗   ██╗████████╗███████╗        ██████╗ ███████╗
+██╔════╝ ██║██╔══██╗████╗  ██║╚══██╔══╝██╔════╝        ██╔══██╗██╔════╝
+██║  ███╗██║███████║██╔██╗ ██║   ██║   ███████╗        ██████╔╝███████╗
+██║   ██║██║██╔══██║██║╚██╗██║   ██║   ╚════██║        ██╔═══╝ ╚════██║
+╚██████╔╝██║██║  ██║██║ ╚████║   ██║   ███████║███████╗██║     ███████║
+ ╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚══════╝╚═╝     ╚══════╝
+                             \033[91m[coded by 9nas_007 ]
+'''
+print banner
+import os
+print ('\033[92m[1].Joomla Tools ')
+print ('\033[92m[2]..Wordpress Tools')
+print ('\033[92m[3]...Drupal Tools ')
+print ('\033[92m[4]....Opencart Tools ')
+print ('\033[92m[5].....magento Tools ')
+print ('\033[92m[6]......After Upload Your Shell ')
+print ('\033[92m[7].......Gs_Boot')
+print ('\033[92m[8]........D.R.S Boot ')
+print ('\033[92m[9].........Local Root Server 2010-2016 ')
+print ('\033[92m[10].........Other Tools ^-^')
+choose = raw_input('\033[93m \n [+]>Choose Tools ^_^ <[+] >>>>:')
+if choose =='1':
+ os.system('git clone https://github.com/hussien-x/fucking_joomla.git ')
+if choose =='2':
+ os.system('git clone https://github.com/9nasghost/simpel-exploit-wordpress.git ')
+if choose =='3':
+ os.system ('git clone https://github.com/9nasghost/Drupal.git')
+if choose =='4':
+  ('git clone https://github.com/9nasghost/opencart-2017.git')
+if choose =='5':
+ os.system ('git clone https://github.com/9nasghost/magento-add-admin.git')
+if choose =='6':
+ os.system ('git clone https://github.com/9nasghost/after-upload-your-shell.git')
+if choose =='7':
+ os.system ('git clone https://github.com/9nasghost/fllaga-boot.git')
+if choose =='8':
+ os.system ('git clone https://github.com/9nasghost/D.R.S-DZ-boot.git')
+if choose =='9':
+ os.system ('git clone https://github.com/FireFart/dirtycow.git')
+if choose =='10':
+ print ('git clone https://github.com/9nasghost/other-tools.git')
+else:
+ print ('\n \033[91m   soory MTHAFAKA .|. -_- .|. ')
